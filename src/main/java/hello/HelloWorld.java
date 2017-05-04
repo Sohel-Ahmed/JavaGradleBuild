7cffd0b6d943ad4b33b38b9e60c0d298a94d0a57
Ahmed <Sohel-Ahmed@users.noreply.github.com>
2017-05-04 14:51:56 -0700

    Update HelloWorld.java

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -4,6 +4,7 @@ import org.joda.time.LocalTime;
 public class HelloWorld {
   public static void main(String[] args) {
     LocalTime currentTime = new LocalTime();
+    System.out.println("Hello GitHub");
     System.out.println("The Current Local time is in UTC standard is: "+ currentTime);
 
     Greeter greeter = new Greeter();
